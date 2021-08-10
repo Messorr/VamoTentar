03ac0a4a4edfc23bb7b90687e2efc251eee2f022
Tiago Rodrigues <tiagotimao@live.com>
2021-08-09 23:16:39 -0300

    css funcionando 3.0

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import { useState } from "react";
 function Home() {
   return (
-    <div className="home">
-      <h1>hellow</h1>
-      <Contador />
-    </div>
+    <section className="view">
+      <nav className="nav">
+        <ul>
+          <li className="nav__links">
+            <a>Quem Sou</a>
+          </li>
+          <li className="nav__links">
+            <a>Projetos</a>
+          </li>
+          <li className="nav__links">
+            <a>Tecnologias Utilizadas</a>
+          </li>
+          <li className="nav__links">
+            <a>Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <Header />
+    </section>
   );
 }
 function Contador() {
@@ -16,6 +31,15 @@ function Contador() {
     <div>
       <div>{contador}</div>
       <button onClick={adicionarContador}>adicionarContador</button>
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div className="header">
+      <p className="header__text">
+        Olá me chamo Tiago Rodrigues Machado,tenhos 25 anos
+      </p>
     </div>
   );
 }
